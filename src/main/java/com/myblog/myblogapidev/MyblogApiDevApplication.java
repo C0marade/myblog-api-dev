2f6ff8cca7b41a70d2ef1efb50b896d9ad4d6636
hzx <2864434784@qq.com>
2022-12-30 20:13:41 +0800

    init project

---
+++ src/main/java/com/myblog/myblogapidev/MyblogApiDevApplication.java
@@ -0,0 +1,13 @@
+package com.myblog.myblogapidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogApiDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogApiDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myblog/myblogapidev/MyblogApiDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.myblogapidev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApiDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
